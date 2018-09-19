8c5c5061818c4745ea755b5c6b7002cd65075aa5
Darren Ladner <dxladner@gmail.com>
2018-09-18 20:26:16 -0500

    footer styling

--- footer.php
+++ footer.php
@@ -15,64 +15,72 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="container">
-			<ul id="footer" class="frow">
-			<li class="fcolumn first top-level"><a href="#" title="Test">Test</a>
-			<ul class="subnav">
-			<li><a href="#" title="Ivie">Test</a></li>
-			<li><a href="#" title="Technology">Test</a></li>
-			<li><a href="#" title="Phones">Test</a></li>
-			<li><a href="#" title="Fax">Test</a></li>
-			<li><a href="#" title="Features">Test</a></li>
-			<li><a href="#" title="Support">Test</a></li>
-			</ul></li>
-			<li class="fcolumn top-level"><a href="#" title="Test">Test</a>
-			<ul class="subnav">
-			<li><a href="#" title="Locate a partner">Test</a></li>
-			<li><a href="#" title="Become a partner">Test</a></li>
-			</ul></li>
-			<li class="fcolumn top-level"><a href="#" title="Test">Test</a>
-			<ul class="subnav">
-			<li><a href="#" title="Contact">Test</a></li>
-			<li><a href="#" title="Test">Test</a></li>
-			<li><a href="#" title="Test">Test</a></li>
-			</ul></li>
-			<li class="fcolumn top-level"><a href="#" title="Test">Test</a>
-			<ul class="subnav">
-			<li><a href="#" title="Blog">Blog</a></li>
-			<li><a href="#" title="Help" target="_blank">Help</a></li>
-			<li><a href="#" title="Test">Test</a></li>
-			</ul></li>
-			</ul>
+			<div class="row prefooter">
+				<div class="col-md-3">
+					test test 
+				</div>
+				<div class="col-md-3">
+					<h5>Recent Posts</h5>
+					<ul>
+						<li>test</li>
+						<li>test</li>
+						<li>test</li>
+					</ul>
+				</div>
+				<div class="col-md-6">
+					<h5 style="text-align:center;">Proudly built using...</h5>
+					<ul id="techicons">
+						<li><i class="fab fa-html5 fa-lg"></i></li>
+						<li><i class="fab fa-css3-alt fa-lg"></i></li>
+						<li><i class="fab fa-sass fa-lg"></i></li>
+						<li><i class="fab fa-js-square fa-lg"></i></li>
+						<li><i class="fab fa-php fa-lg"></i></li>
+						<li><i class="fab fa-wordpress fa-lg"></i></li>
+						<li><i class="fab fa-laravel fa-lg"></i></li>
+						<li><i class="fab fa-react fa-lg"></i></li>
+						<li><i class="fas fa-code-branch fa-lg"></i></li>
+						
+					</ul>
+				</div>
+			</div>
 		</div><!--  .container -->
 		<div class="frow social">
-  <div class="fcolumn logo">
-    <img src="/images/logo.svg" class="HyperdriveDesigns Logo">
-  </div>
+			<div class="fcolumn logo">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/img/hdLogosmall.png" />
+				</a>
+			</div>
 
-  <div class="fcolumn icons">
-    <ul>
-      
-              <li><a href="https://www.facebook.com/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
-      
-              <li><a href="https://twitter.com/"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-      
-              <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
-          </ul>
-  </div>
-</div>
-<div class="frow disclaimer">
-  <div class="fcolumn copyright">
-    <p>Copyright <i class="fa fa-copyright" aria-hidden="true"></i> 2018 Hyperdrive Designs</p>
-  </div>
+			<div class="fcolumn icons">
+				<ul id="socialicons">
+						<li><a href="https://www.facebook.com/"><i class="fab fa-twitter fa-2x"></i></a></li>
 
-  <div class="fcolumn pages">
-    <ul>
-      <li><a href="/legal-stuff/customer-terms-of-service-agreement">Legal Stuff</a></li>
-      <li><a href="/legal-stuff/privacy-policy">Privacy Policy</a></li>
-      <li><a href="/sitemap.html">Site Map</a></li>
-    </ul>
-  </div>
-</div>
+						<li><a href="https://twitter.com/"><i class="fab fa-bitbucket fa-2x"></i></a></li>
+
+						<li><a href="#"><i class="fab fa-github fa-2x"></i></a></li>
+
+						<li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in fa-2x"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="frow subfooter">
+			<div class="col-md-4 copyright">
+				<p>Copyright <i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date('Y'); ?> Hyperdrive Designs</p>
+			</div>
+			<div class="col-md-4 seal">
+				<span id="siteseal">
+					<script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=Ri3x7GvXAxj9GpmERJc9TllYRLu56x5P5WwhfS5ZzbbNkM0jj6FSj3nHClP">
+					</script>
+					<!-- <img style="cursor:pointer;cursor:hand" src="https://seal.godaddy.com/images/3/en/siteseal_gd_3_h_d_m.gif" onclick="verifySeal();" alt="SSL site seal - click to verify"></span> -->
+			</div>
+			<div class="col-md-4 legal">
+				<ul>
+					<li><a href="/legal-stuff/customer-terms-of-service-agreement">Legal Stuff</a></li>
+					<li><a href="/legal-stuff/privacy-policy">Privacy Policy</a></li>
+					<li><a href="/sitemap.html">Site Map</a></li>
+				</ul>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
